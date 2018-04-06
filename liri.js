0f0b1a15f5f18c5bc509e68c4ba9ed7603fcbc72
davidlatuno <dslatuno@gmail.com>
2018-04-05 17:24:28 -0700

    Added keys variables to lir.js

--- liri.js
+++ liri.js
@@ -2,3 +2,6 @@ require("dotenv").config();
 var Twitter = require("twitter");
 var Spotify = require("node-spotify-api");
 var request = require("request");
+var apiKeys = require("./keys.js");
+var spotify = new Spotify(apiKeys.spotify);
+var client = new Twitter(keys.twitter);
