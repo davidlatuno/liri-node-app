c544ab6e9d10bab1a9f4bbc30442c2f57b6c508d
davidlatuno <dslatuno@gmail.com>
2018-04-05 19:13:53 -0700

    Initial tweet functionality

--- liri.js
+++ liri.js
@@ -4,4 +4,28 @@ var Spotify = require("node-spotify-api");
 var request = require("request");
 var apiKeys = require("./keys.js");
 var spotify = new Spotify(apiKeys.spotify);
-var client = new Twitter(keys.twitter);
+var client = new Twitter(apiKeys.twitter);
+
+var command = process.argv[2];
+
+var params = {
+    q: 'davidlatuno',
+    count: 20
+}
+
+if (command === "my-tweets") {
+
+    client.get('search/tweets', params, function searchedData(err, data, response) {
+        if (err) {
+            return console.log(err);
+        }
+
+        for (var i = 0; i < data.statuses.length; i++) {
+            console.log(data.statuses[i].text);
+            console.log(data.statuses[i].created_at);
+            console.log("");
+        }
+
+
+    });
+}
